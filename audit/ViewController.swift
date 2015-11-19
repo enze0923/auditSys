e2c788890a449fc5caf440b403261e6f30786f26
enze <felix.wu@bwcmall.com>
2015-11-19 17:20:37 +0800

    Initial Commit

---
+++ audit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  audit
+//
+//  Created by 武恩泽 on 15/11/19.
+//  Copyright © 2015年 武恩泽. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
